ca68fb369636eefc47a5635605aeec06ef3481c4
Hampton Abbott <228664908+HPAuncc@users.noreply.github.com>
2026-01-18 16:21:04 -0500

    Add layout and title to projects page
    Added front matter for layout and title to projects page.

---
+++ projects.m
@@ -0,0 +1,16 @@
+---
+layout: page
+title: Projects
+permalink: /projects/
+---
+
+## Portfolio/Projects
+This section will be updated as the semester progresses. Below is the structure for my upcoming work:
+
+### Featured Projects
+* **Project Title (Coming Soon)**
+    * **Problem:** Brief description of the issue.
+    * **Data:** Sources and types used.
+    * **Method:** Tools and algorithms applied.
+    * **Result:** Key findings or outcomes.
+    * **Link:** [Repo Link]
